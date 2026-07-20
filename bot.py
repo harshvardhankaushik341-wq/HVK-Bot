b34a08e759d9c93fefc2b131050349b796797d28
harshvardhankaushik341-wq <harshvardhankaushik341@gmail.com>
2026-07-20 19:46:56 +0530

    Create bot.py
    bot.py created

---
+++ bot.py
@@ -0,0 +1,18 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"{bot.user} is online!")
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("🏓 Pong!")
+
+bot.run("YOUR_BOT_TOKEN")
